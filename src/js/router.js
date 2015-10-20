d0f1aca5d351ab9a0ccd740b69a4ad121cd9e958
Davy Peter Braun <hello@davybraun.com>
2015-10-20 20:39:17 +0200

    Update router to use MangoSpaView.

--- src/js/router.js
+++ src/js/router.js
@@ -1,5 +1,5 @@
 var Backbone = require('backbone');
-var RootView = require('./views/root.view');
+var MangoSpaView = require('./views/mangospa.view');
 
 var Router = Backbone.Router.extend({
   routes: {
@@ -11,11 +11,23 @@ var Router = Backbone.Router.extend({
   },
 
   default: function() {
-    var view = new RootView({
-      target: '#limesurvey'
+    var rootView = new MangoSpaView({
+      el: '#mango-spa-root'
     });
-    view.render();
-  }
+
+    var success = function (data) {
+      console.log('success');
+      console.log(data);
+    }
+
+    var failure = function () {
+      console.log('failure');
+    }
+
+    rootView
+      .createWelcomeScreen('#limesurvey', success, failure)
+      .renderWelcomeScreen();
+    }
 });
 
 module.exports = Router;
